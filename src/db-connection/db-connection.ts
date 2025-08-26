6972fce06e036c6ac3b7c3e9cd586dc0a04f0ca8
Md. Shahjalal <shahjalaldk@gmail.com>
2025-08-26 10:52:22 +0600

    Update db-connection.ts

--- src/db-connection/db-connection.ts
+++ src/db-connection/db-connection.ts
@@ -11,6 +11,7 @@ const createDBConnection = async () => {
     const MONGO_DB_NAME = config.get<string>("MONGO_DB_NAME");
     const MONGO_PORT = config.get<string>("MONGO_PORT");
 
+    //mongodb connection information
     const mongoConnectionUri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
     const mongoDbConnectionURI = NODE_ENV === "local_development" ? `mongodb://localhost:${MONGO_PORT}/${MONGO_DB_NAME}` : (NODE_ENV === "development" ? mongoConnectionUri : mongoConnectionUri);
 
